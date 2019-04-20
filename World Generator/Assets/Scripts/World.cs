652b10de9f347a1dd9f7c78df6f3a47345a14d13
Zalron <Davidrlawson@optusnet.com.au>
2019-04-20 18:48:41 +1000

    The First Chunk

---
+++ World Generator/Assets/Scripts/World.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World : MonoBehaviour
+{
+    Chunk chunk;
+    public static Matrix4x4 id = Matrix4x4.identity;
+    public Material material;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        chunk = new Chunk(new Vector3Int(0,0,0));
+        chunk.GenerateBlockArray();
+        StartCoroutine(chunk.GenerateMesh());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (chunk.ready)
+        {
+            Graphics.DrawMesh(chunk.mesh, id, material, 0);
+        }
+    }
+
+    public void Draw()
+    {
+       
+    }
+}
